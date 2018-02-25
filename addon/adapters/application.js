440be3960cdb8f432696fabfbb37989f46d703ab
Dan Freeman <dfreeman@salsify.com>
2018-02-25 18:32:04 -0500

    [WIP] versioned deploys

--- addon/adapters/application.js
+++ addon/adapters/application.js
@@ -1,12 +1,9 @@
-import { inject as service } from '@ember/service';
 import DS from 'ember-data';
 import config from 'dummy/config/environment';
 
 export default DS.Adapter.extend({
 
   namespace: `${config.rootURL.replace(/\/$/, '')}/docs`,
-
-  projectVersion: service(),
 
   project: null,
 
